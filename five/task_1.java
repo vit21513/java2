ca1fbf25b9c5e80ab2cc23b3c61cb3f003a22489
vit21513 <vit21513@yandex.ru>
2022-12-07 20:47:20 +0300

    temp

---
+++ five/task_1.java
@@ -0,0 +1,26 @@
+// 1 Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
+
+
+package five;
+
+
+public class task_1 {
+
+    public static void main(String[] args) {
+
+        
+    }
+
+   
+   
+   
+   
+   
+    }
+
+
+
+
+
+
+
---
+++ five/task_2.java
@@ -0,0 +1,43 @@
+package five;
+
+// Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности.
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class task_2 {
+
+    public static void main(String[] args) {
+
+       
+       Map<String, Integer> name = new HashMap<>();
+        String[] lastName = {
+                "Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина", "Анна Крутова", "Иван Юрин",
+                "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова", "Марина Светлова", "Мария Савина",
+                "Мария Рыкова", "Марина Лугова", "Анна Владимирова", "Иван Мечников", "Петр Петин", "Иван Ежов"
+        };
+
+        for (int i = 0; i < lastName.length; i++) {
+            String[] parts = lastName[i].split(" ");
+            if (name.containsKey(parts[0]))
+                name.put(parts[0], name.get(parts[0]) + 1);
+            else {name.put(parts[0], 1);}
+            }
+           
+
+        Map<String, Integer> sortedMap = name.entrySet().stream()
+        .sorted(Comparator.comparingInt(e -> -e.getValue()))
+        .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(a, b) -> { throw new AssertionError(); },
+        LinkedHashMap::new
+        ));
+        
+        System.out.println(sortedMap);
+    
+}
+   
+     
+}
