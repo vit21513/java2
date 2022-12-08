cb5639f29d96d92315d6535f17060d92e9ef76f1
vit21513 <vit21513@yandex.ru>
2022-12-08 20:35:32 +0300

    fin

--- five/task_1.java
+++ five/task_1.java
@@ -1,26 +1,38 @@
 // 1 Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
 
-
 package five;
 
-
+import java.util.HashMap;
+import java.util.Map;
 public class task_1 {
 
     public static void main(String[] args) {
 
-        
+        //
+        Map<String, String> phoneboook = new HashMap<>();
+
+        String[] listTelephone = {
+                "Иван Иванов 892215131", "Светлана Петрова 8963614515", "Иван Мечников 898855915855", "Анна Мусина 899555666211",
+                "Анна Крутова 89555414185", "Иван Юрин 858552156",
+                "Петр Лыков 8965585652", "Павел Чернов 845655956", "Петр Чернышов 82552854252", "Иван Иванов 8458552811",
+                "Марина Светлова 8451251515", "Мария Савина 8650123457",
+                "Мария Рыкова 84555219656", "Иван Иванов 8456555956", "Анна Владимирова 8786955256", "Иван Мечников 825558855",
+                "Петр Петин 812525555521", "Иван Ежов 81452565252" };
+
+
+
+        for (int i = 0; i < listTelephone.length; i++) {
+            String[] parts = listTelephone[i].split(" ");
+            String nam = (parts[0] + " " + parts[1]);
+            if (phoneboook.containsKey(nam)) {
+                phoneboook.put(nam, phoneboook.get(nam) + " " + parts[2]);
+            } else {
+                phoneboook.put(nam, parts[2]);
+            }
+
+        }
+        phoneboook.forEach((k, v) -> {System.out.printf("%s: %s%n", k, v);});
+
     }
 
-   
-   
-   
-   
-   
-    }
-
-
-
-
-
-
-
+}
--- five/task_2.java
+++ five/task_2.java
@@ -8,13 +8,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
 public class task_2 {
 
     public static void main(String[] args) {
 
-       
-       Map<String, Integer> name = new HashMap<>();
+        Map<String, Integer> name = new HashMap<>();
         String[] lastName = {
                 "Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина", "Анна Крутова", "Иван Юрин",
                 "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова", "Марина Светлова", "Мария Савина",
@@ -25,19 +23,22 @@ public class task_2 {
             String[] parts = lastName[i].split(" ");
             if (name.containsKey(parts[0]))
                 name.put(parts[0], name.get(parts[0]) + 1);
-            else {name.put(parts[0], 1);}
+            else {
+                name.put(parts[0], 1);
             }
-           
+        }
 
-        Map<String, Integer> sortedMap = name.entrySet().stream()
-        .sorted(Comparator.comparingInt(e -> -e.getValue()))
-        .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(a, b) -> { throw new AssertionError(); },
-        LinkedHashMap::new
-        ));
-        
-        System.out.println(sortedMap);
-    
-}
-   
-     
+        Map<String, Integer> sortedMap = name.entrySet().stream().sorted(Comparator.comparingInt(e -> -e.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> {
+                    throw new AssertionError();
+                }, LinkedHashMap::new));
+
+        sortedMap.forEach((k, v) -> {
+            if (sortedMap.get(k) > 1) { // для вывода только вторяющихся имен т.е больше 1го
+                System.out.printf("%s: %s%n", k, v);
+            }
+        });
+
+    }
+
 }
